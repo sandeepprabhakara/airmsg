260bf8fb7e6dc0a78ab59e54afd1831a8d0eabf1
Sandeep Pabhakara <sandeep.prabhakara@gmail.com>
2013-12-23 21:47:49 -0500

    Updated the files slightly to show responder and initiator counts on the home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Pinteresting::Application.routes.draw do
 
   resources :pins
 
+  devise_for :users
   resources :users do
     member do
       get :responders, :initiators
     end
   end
 
-
-  devise_for :users
   root "pins#index"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
